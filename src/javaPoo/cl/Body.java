8eb8f6fbaa940b8c252e1820d65060f206a856fe
Yekpahi Prince <princeyekpahi@hotmail.fr>
2022-09-14 12:00:59 +0200

    second commit

--- src/javaPoo/cl/Body.java
+++ src/javaPoo/cl/Body.java
@@ -1,5 +1,9 @@
 package javaPoo.cl;
 
 public class Body {
-
+	protected String nom;
+	
+	public void myOrg() {
+		System.out.println("Je suis le coeur!!");
+	}
 }
--- src/javaPoo/cl/Main.java
+++ src/javaPoo/cl/Main.java
@@ -1,54 +1,15 @@
 package javaPoo.cl;
 
-/*class Car {
-	public String name;
-	
-	public void present() {
-		System.out.println("My name is " + name);
-	}
-}
-*/
-class Encap {
-	private String ville;
-	
-	public String getVille () {
-		return ville;
-	}
-	
-	public String setVille (String nomVille) {
-		return this.ville = nomVille;
-	}
-}
+public class Main extends Body {
 
-public class Main {
-	/*
-	int a;
-	
-	public Main(int c) {
-		this.a = c;
-	}
-	static void myMethod() {
-	    System.out.println("Hello World!");
-	  }
-	*/
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	/*	
-		Car c1 = new Car();
-		c1.name = "Belley";
-		c1.present();
-		System.out.println(c1.name);
-		 myMethod();
-		 Main bx = new Main(10);
-		 int z = bx.a;
-		 System.out.println(z);
-		 */
+
+		Main org = new Main();
+		org.nom = "Bras";
 		
-		Encap en1 = new Encap();
+		org.myOrg();
 		
-		en1.setVille("Man");
-		 System.out.println(en1.getVille());
-		
+		System.out.println(org.nom);
 	}
 
 }
