230e5f1ed350e08e71570ebee567ff3bcf0a3499
Yekpahi Prince <princeyekpahi@hotmail.fr>
2022-09-15 12:02:24 +0200

    Méthode générique

--- src/javaPoo/cl/Main.java
+++ src/javaPoo/cl/Main.java
@@ -1,28 +1,24 @@
 package javaPoo.cl;
 
-class Cat implements Animal {
-
-	
-	public void play() {
-		System.out.println("Les chats aiment jouer sauter sauter!!");
-	}
-
-	public String dance() {
-		String danse = "Gbegbe";
-		return "J'aime danser " + danse;
-	}
-	
-}
 
 
 public class Main {
 
+	
+	public static <E> void display(E[] tab) {
+		for (E e : tab) {
+			System.out.printf("%s", e);
+		}
+	}
 	public static void main(String[] args) {
 
-	Cat c1 = new Cat();
-	
-	c1.play();
-	System.out.println(c1.dance());
+		Integer[] i = {1, 2, 3};
+		System.out.println("Tableau des entiers : ");
+		display(i);
+		
+		String[] c = {"A", "B", "C"};
+		System.out.println("\nTableau des caractères : ");
+		display(c);
 	}
 
 }
